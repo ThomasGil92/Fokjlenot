f9ead1e5fdaea3838782624dc86bfb1a1711d92d
ThomasGil92 <tgil849@gmail.com>
2020-08-26 09:39:41 +0200

    deploy

--- client/src/components/LandingPage/LandingFooter.js
+++ client/src/components/LandingPage/LandingFooter.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const LandingFooter = () => {
     return (
-        <nav className="navbar navbar-dark d-flex justify-content-center text-center px-0 mt-md-5 w-100 todoTitleFont" style={{ left: "0", backgroundColor: "#03224c" }}>
+        <nav className="navbar navbar-dark d-flex justify-content-center text-center px-0  position-absolute mt-md-5  col-12 w-100 todoTitleFont" style={{ left: "0", backgroundColor: "#03224c" }}>
             <a href="https://fokjlenot.herokuapp.com/legal" className="btn btn-outline-light py-1" style={{ fontSize: "0.9em" }}>Mentions légales</a>
             <a href="#landingTop" className="btn btn-outline-light py-1" style={{ fontSize: "0.9em" }}>Haut de la page</a>
 
--- client/src/components/LandingPage/LandingHeader.js
+++ client/src/components/LandingPage/LandingHeader.js
@@ -10,7 +10,7 @@ const LandingHeader = () => {
                 backgroundRepeat: "no-repeat",
                 backgroundImage: `url(img/landingHeader.jpg)`,
             }}>
-            <div className="col-12 d-flex align-items-center justify-content-center text-center">
+            <div className="col-12 d-flex align-items-center justify-content-center text-center px-auto">
                 <div className="col-12 col-md-6 ">
                     <p className="lead pt-5 mt-5 mb-3" style={{ fontSize: "1.1em" }}>L'essentiel pour la gestion de vos projets</p>
                     <Link to={"/signup"} className="btn btn-primary mb-3" style={{ fontSize: "1.1em" }}>S'inscrire</Link>
