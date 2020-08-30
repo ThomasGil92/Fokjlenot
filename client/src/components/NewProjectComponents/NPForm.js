60f509cff1fdeea9b47dabc357e00eeb8eb3da38
ThomasGil92 <tgil849@gmail.com>
2020-08-30 10:24:47 +0200

    deploy v2.1

--- client/src/components/NewProjectComponents/NPForm.js
+++ client/src/components/NewProjectComponents/NPForm.js
@@ -25,7 +25,7 @@ const NPForm = () => {
     }
 const focus=()=>{
     if(window.innerWidth <= 700){
-        document.scrollIntoView(alignToTop);
+        document.scrollIntoView(true);
     }
 }
     const handleSubmit = e => {
