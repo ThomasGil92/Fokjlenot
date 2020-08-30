0b5728ce9409351d5efcd21cfb807f31390274ec
ThomasGil92 <tgil849@gmail.com>
2020-08-30 10:33:28 +0200

    deploy v2.1

--- client/src/components/NewProjectComponents/NPForm.js
+++ client/src/components/NewProjectComponents/NPForm.js
@@ -25,7 +25,7 @@ const NPForm = () => {
     }
 const focus=()=>{
     if(window.innerWidth <= 700){
-        document.scrollIntoView({block:"end"});
+        document.getElementsByTagName("input").scrollIntoView({block:"center"});
     }
 }
     const handleSubmit = e => {
