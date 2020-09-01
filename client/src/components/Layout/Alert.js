ed2a7c197ba3cfcfd69c7fe8906b8ef11a93bc56
ThomasGil92 <tgil849@gmail.com>
2020-09-01 10:48:18 +0200

    deploy v2.1

--- client/src/components/Layout/Alert.js
+++ client/src/components/Layout/Alert.js
@@ -8,7 +8,7 @@ const Alert = () => {
     return (
         alerts.length ? (alerts.map((alert, i) => {
             return (
-                <div key={i} className={`alert my-3 col-12 col-md-10 mx-auto fixed-top mx-auto alert-${alert.alertType}`} style={{
+                <div key={i} className={`alert position-absolute col-12 col-md-10 mx-auto fixed-top mx-auto alert-${alert.alertType}`} style={{
                     zIndex: "500", top: "20px"
                 }}>
                     <p className="lead text-dark m-0 text-center" >{alert.msg}</p>
