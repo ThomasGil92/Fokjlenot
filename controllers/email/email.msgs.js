e668d7d69e23481adfe24f902244b09531b4473f
ThomasGil92 <tgil849@gmail.com>
2020-09-02 10:29:22 +0200

    deploy

--- controllers/email/email.msgs.js
+++ controllers/email/email.msgs.js
@@ -1,5 +1,5 @@
 module.exports = {
-    confirm: 'Un email vous à été envoyé dans votre boite Mail pour confirmation.',
+    confirm: 'Un email vous a été envoyé dans votre boite Mail pour confirmation (vérifiez vos spams).',
     confirmed: 'Votre adresse email a bien été confirmée!',
     resend: 'Un nouveau message de confirmation vient de vous être renvoyé, vérifiez vos spams',
     couldNotFind: 'Nous ne vous trouvons pas dans notre base de donnée',
